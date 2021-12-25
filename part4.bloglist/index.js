0f856ee13b6a43d6373d6a6f4911c91e0ef5b367
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2021-12-25 22:59:06 +0100

    ex 4.2

--- part4.bloglist/index.js
+++ part4.bloglist/index.js
@@ -1,94 +1,50 @@
-require('dotenv').config()
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const url = process.env.MONGODB_URI
+const server = http.createServer(app)
 
-mongoose.connect(url)
-
-app.use(cors())
-app.use(express.json())
-
-
-
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-  // const body = request.body
 
-  // if (body.title === undefined) {
-  //   return response.status(400).json({error: 'title missing'})
+// const http = require('http')
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
+// const Blog = require('./models/blog')
 
-  // }
-  // const blog = new Blog({
-  //   title: body.title,
-  //   author: body.author,
-  //   url: body.url,
-  //   likes: body.likes
+// const url = process.env.MONGODB_URI
 
-  // })
+// mongoose.connect(url)
 
-  // blog
-  //   .save()
-  //   .then(savedBlog => {
-  //     response.json(savedBlog)
-  //   })
-})
+// app.use(cors())
+// app.use(express.json())
 
-app.get('/api/blogs/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-  .then(blog => {
-    if (blog) {
-      response.json(blog)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error=>next(error))
-})
 
-app.delete('/api/blogs/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(result => {
-      console.log(request.params.id)
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'})
-  }
-  next(error)
-}
 
-app.use(errorHandler)
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// const errorHandler = (error, request, response, next) => {
+//   console.error(error.message)
+
+//   if (error.name === 'CastError') {
+//     return response.status(400).send({error: 'malformatted id'})
+//   }
+//   next(error)
+// }
+
+// app.use(errorHandler)
+
+// const PORT = process.env.PORT
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
 
 
 
